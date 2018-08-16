8921e492ce66a85caf946124150daedc6d671445
Steve Penrod <steve.penrod@mycroft.ai>
2018-08-15 23:35:05 -0500

    Add major version upgrade agreement mechanism
    Interim checkin for a mechanism that gets user approval when there is a
    major version change (e.g. 18.02.xx -> 18.08.xx).  When detected, the
    user hears more details about the impact and is asked for confirmation
    before the upgrade is allowed to continue.  This also updates the
    USER/mycroft.conf with the "max_allowed_core_version" after the agreement,
    which is used in the update cron job thereafter.
    
    Also cleaned up some of the function calls and improved pronunciation of
    the version numbers.

--- __init__.py
+++ __init__.py
@@ -19,7 +19,9 @@ from adapt.intent import IntentBuilder
 
 from mycroft import Message
 from mycroft.skills.core import MycroftSkill
-from mycroft.version import CORE_VERSION_STR
+from mycroft.version import (
+    CORE_VERSION_MAJOR, CORE_VERSION_MINOR, CORE_VERSION_BUILD,
+    CORE_VERSION_STR )
 
 
 class VersionCheckerSkill(MycroftSkill):
@@ -41,30 +43,55 @@ class VersionCheckerSkill(MycroftSkill):
     def find_version(version_str):
         return list(map(int, version_str.split('.')))
 
+    @staticmethod
+    def ver_data(version_list):
+        return {'major': version_list[0],
+                'minor': version_list[1],
+                'build': version_list[2]}
+
     def check_version(self, message):
         # Report the version of mycroft-core software
-        cur_ver = self.find_version(CORE_VERSION_STR)
 
-        # alphaa fixes Mimic pronunciation
-        postfix = 'alphaa' if cur_ver < [1, 0, 0] else 'beta'
-
+        # display the version on the device screen and speak it
+        cur_ver = [CORE_VERSION_MAJOR, CORE_VERSION_MINOR, CORE_VERSION_BUILD]
         self.enclosure.deactivate_mouth_events()
-        self.enclosure.mouth_text(CORE_VERSION_STR + postfix[0])
-        data = {'version': CORE_VERSION_STR + ' ' + postfix}
-        self.speak_dialog('version', data)
+        self.enclosure.mouth_text(CORE_VERSION_STR + "b")  # b for Beta
+        self.speak_dialog('version', self.ver_data(cur_ver))
+        mycroft.util.wait_while_speaking()
+        self.enclosure.activate_mouth_events()
 
         try:
+            # Obtain the most recent release's verion # from Github
             request = requests.get(self.RELEASE_URL)
             new_ver_str = request.json()['tag_name'].replace('release/v', '')
             new_ver = self.find_version(new_ver_str)
+            new_ver = [18,8,0]  # TODO: Testing only!!!!!
+
+            allowed_ver = self.config_core.get("max_allowed_core_version",
+                                               cur_ver)
+
+            self.log.info("Current version: "+str(cur_ver))
+            self.log.info("Latest version: "+str(new_ver))
+            self.log.info("Allowed version: "+str(allowed_ver))
 
             if cur_ver == new_ver:
                 self.speak_dialog('version.latest')
+            elif new_ver > allowed_ver:
+                # Get user permission for major upgrade
+                resp = self.ask_yesno('major.upgrade',
+                                      data=self.ver_data(new_ver))
+                if resp == 'yes':
+                    # TODO write the local value str(new_ver[0])+"."+str(new_ver[1])
+                    self.save_upgrade_permission(new_ver)
+                    self.speak_dialog('upgrade.started')
+                    self.emitter.emit(Message('system.update'))
+                else:
+                    self.speak_dialog('major.upgrade.declined')
             elif cur_ver < new_ver:
-                self.speak_dialog('version.older', {'new_version': new_ver_str})
-                yes_words = set(self.translate_list('yes'))
-                should_upgrade = self.get_response('ask.upgrade')
-                if any(word in should_upgrade.split() for word in yes_words):
+                # Ask user if they want to update immediately
+                resp = self.ask_yesno('ask.upgrade',
+                                      data={'new_version': new_ver_str})
+                if resp == 'yes':
                     self.speak_dialog('upgrade.started')
                     self.emitter.emit(Message('system.update'))
                 else:
@@ -76,8 +103,20 @@ class VersionCheckerSkill(MycroftSkill):
         # NOTE: intentionally sticking with this deprecated API instead
         # of mycroft.audio.wait_while_speaking() so that this skill
         # works on 0.8.15+
-        mycroft.util.wait_while_speaking()
-        self.enclosure.activate_mouth_events()
+
+    def save_upgrade_permission(self, ver):
+        from mycroft.configuration.config import (
+            LocalConf, USER_CONFIG, Configuration
+        )
+
+        float_ver = ver[0] + ver[1]/10
+        new_conf_values = { "max_allowed_core_version": float_ver }
+
+        user_config = LocalConf(USER_CONFIG)
+        user_config.merge(new_conf_values)
+        user_config.store()
+
+        self.emitter.emit(Message('configuration.updated'))
 
     def check_platform_build(self, message):
         if 'platform_build' in self.config_core['enclosure']:
